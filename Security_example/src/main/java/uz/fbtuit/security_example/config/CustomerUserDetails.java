7240cb24be37cd334270d9f710ed91e4ea90f238
Jah0ng1r <rajabaliyevjahongir2@gmail.com>
2022-11-30 09:37:17 +0500

    Security Start but promlem yeah

--- Security_example/src/main/java/uz/fbtuit/security_example/config/CustomerUserDetails.java
+++ Security_example/src/main/java/uz/fbtuit/security_example/config/CustomerUserDetails.java
@@ -1,11 +1,17 @@
 package uz.fbtuit.security_example.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import uz.fbtuit.security_example.entity.MyUsers;
+import uz.fbtuit.security_example.entity.Role;
 import uz.fbtuit.security_example.repository.MyUsersRepository;
+
+import java.util.ArrayList;
 
 public class CustomerUserDetails implements UserDetailsService {
     private final MyUsersRepository myUsersRepository;
@@ -21,6 +27,15 @@ public class CustomerUserDetails implements UserDetailsService {
         MyUsers user = myUsersRepository.findByUserName(username);
         if (user == null) throw new UsernameNotFoundException("User Topilmadi");
 
-        return null;
+        return new User(user.getUserName(), user.getPassword(), getRole(user));
+    }
+
+    private ArrayList<GrantedAuthority> getRole(MyUsers user) {
+        ArrayList<GrantedAuthority> list = new ArrayList<>();
+        for (Role role : user.getRoleSet()) {
+            list.add(new SimpleGrantedAuthority(role.getName().name()));
+
+        }
+        return list;
     }
 }
--- Security_example/src/main/java/uz/fbtuit/security_example/config/SecurityConfig.java
+++ Security_example/src/main/java/uz/fbtuit/security_example/config/SecurityConfig.java
@@ -1,9 +1,14 @@
 package uz.fbtuit.security_example.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfiguration {
@@ -12,5 +17,21 @@ public class SecurityConfig extends WebSecurityConfiguration {
     @Autowired
     public SecurityConfig(UserDetailsService userDetailsService) {
         this.userDetailsService = userDetailsService;
+    }
+
+    @Autowired
+    public void connfigureGlobalSeurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
     }
 }
